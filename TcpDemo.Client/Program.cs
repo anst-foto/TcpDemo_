b589fbd466532fa4ea40f9b346f8578dfd32360f
Starinin Andrey <starinin-andrey@ya.ru>
2025-05-22 20:37:23 +0300

    Initial commit

---
+++ TcpDemo.Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+var endPoint = new IPEndPoint(IPAddress.Loopback, 50000);
+var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+await server.ConnectAsync(endPoint);
+
+while (true)
+{
+    var buffer = new byte[1024];
+    var size = await server.ReceiveAsync(buffer);
+    var text = Encoding.UTF8.GetString(buffer, 0, size);
+    Console.WriteLine(text);
+    
+    await Task.Delay(100);
+    
+    Console.Write("> ");
+    var messageText = Console.ReadLine();
+    var message = Encoding.UTF8.GetBytes(messageText);
+    await server.SendAsync(message);
+    
+    if (messageText == "exit") break;
+}
+
+server.Close();
---
+++ TcpDemo.Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+var endPoint = new IPEndPoint(IPAddress.Loopback, 50000);
+var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+listener.Bind(endPoint);
+listener.Listen(10);
+
+var client = await listener.AcceptAsync();
+
+while (true)
+{
+    var messageText = "Hello World!";
+    var message = Encoding.UTF8.GetBytes(messageText);
+    await client.SendAsync(message);
+    
+    await Task.Delay(100);
+    
+    var buffer = new byte[1024];
+    var size = await client.ReceiveAsync(buffer);
+    var text = Encoding.UTF8.GetString(buffer, 0, size);
+    Console.WriteLine(text);
+    
+    if (text == "exit") break;
+}
+
+client.Close();
+listener.Close();
